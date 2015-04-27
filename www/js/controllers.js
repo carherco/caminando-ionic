ab5d9a8cdb4fb195491caca8eefb6bd4d86bdc15
carherco <carherco@gmail.com>
2015-04-27 23:51:57 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,238 @@
+var server_url = 'http://localhost/caminando/web/app_dev.php';
+
+var url_matrimonios_get = server_url + '/matrimonios/transito7';
+var url_solteros_get = server_url + '/solteros/transito7';
+var url_ausentes_get = server_url + '/ausentes/transito7';
+
+var url_matrimonios_post = server_url + '/matrimonios/transito7';
+var url_solteros_post = server_url + '/solteros/transito7';
+var url_ausentes_post = server_url + '/ausentes/transito7';
+
+var url_matrimonios_delete = server_url + '/matrimonios/transito7/';
+var url_solteros_delete = server_url + '/solteros/transito7/';
+var url_ausentes_delete = server_url + '/ausentes/transito7/';
+
+var url_hermanos_put = server_url + '/hermanos/put/transito7/';
+
+angular.module('grupos.controllers', [])
+
+.controller('HermanosCtrl', function ($scope, $http) {
+
+    $scope.matrimonios = [];
+    $scope.solteros = [];
+    $scope.bajan_poco = [];
+    $scope.ausentes = [];
+
+    $http.get(url_matrimonios_get).success(function (data) {
+        $scope.matrimonios = data;
+    });
+
+    $http.get(url_solteros_get).success(function (data) {
+        $scope.solteros = data;
+    });
+
+    $http.get(url_ausentes_get).success(function (data) {
+        $scope.ausentes = data;
+    });
+
+    $scope.showMatrimonioForm = false;
+    $scope.matrimonio_nuevo = {};
+    $scope.addMatrimonio = function () {
+        $http.post(url_matrimonios_post, {nombre: $scope.matrimonio_nuevo.nombre}).success(function (data) {
+            $scope.matrimonio_nuevo = {'id': data.id, 'nombre': data.nombre};
+            $scope.matrimonios.push($scope.matrimonio_nuevo);
+            $scope.matrimonio_nuevo = {};
+        });
+    };
+    $scope.deleteMatrimonio = function (index) {
+        var id = $scope.matrimonios[index].id;
+        $http.delete(url_matrimonios_delete + id).success(function (data) {
+            $scope.matrimonios.splice(index, 1);
+        });
+    };
+
+    $scope.showSolteroForm = false;
+    $scope.soltero_nuevo = {};
+    $scope.addSoltero = function () {
+        $http.post(url_solteros_post, {nombre: $scope.soltero_nuevo.nombre}).success(function (data) {
+            $scope.soltero_nuevo = {'id': data.id, 'nombre': data.nombre};
+            $scope.solteros.push($scope.soltero_nuevo);
+            $scope.soltero_nuevo = {};
+        });
+    };
+    $scope.deleteSoltero = function (index) {
+        var id = $scope.solteros[index].id;
+        $http.delete(url_solteros_delete + id).success(function (data) {
+            $scope.solteros.splice(index, 1);
+        });
+    };
+
+    $scope.ausentarMatrimonio = function (index) {
+        var id = $scope.matrimonios[index].id;
+        $http.post(url_hermanos_put + id, {ausente: 1}).success(function (data) {
+            $scope.ausentes.push($scope.matrimonios[index]);
+            $scope.matrimonios.splice(index, 1);
+        });
+    };
+    $scope.ausentarSoltero = function (index) {
+        var id = $scope.solteros[index].id;
+        $http.post(url_hermanos_put + id, {ausente: 1}).success(function (data) {
+            $scope.ausentes.push($scope.solteros[index]);
+            $scope.solteros.splice(index, 1);
+        });
+    };
+    $scope.desAusentar = function (index) {
+        var id = $scope.ausentes[index].id;
+        $http.post(url_hermanos_put + id, {ausente: 0}).success(function (data) {
+            if (data.tipo === 'matrimonio') {
+                $scope.matrimonios.push($scope.ausentes[index]);
+            } else if (data.tipo === 'soltero') {
+                $scope.solteros.push($scope.ausentes[index]);
+            }
+            $scope.ausentes.splice(index, 1);
+        });
+    };
+
+    $scope.deleteAusente = function (index) {
+        var id = $scope.ausentes[index].id;
+        $http.delete(url_ausentes_delete + id).success(function (data) {
+            $scope.ausentes.splice(index, 1);
+        });
+    };
+
+
+
+
+
+
+})
+
+.controller('GruposCtrl', function ($scope, $http) {
+    
+    $scope.matrimonios = [];
+    $scope.solteros = [];
+    $scope.bajan_poco = [];
+    $scope.ausentes = [];
+
+    $http.get(url_matrimonios_get).success(function (data) {
+        $scope.matrimonios = data;
+    });
+
+    $http.get(url_solteros_get).success(function (data) {
+        $scope.solteros = data;
+    });
+
+    $http.get(url_ausentes_get).success(function (data) {
+        $scope.ausentes = data;
+    });
+    
+    
+    $scope.numPersonas = function () {
+        return 2 * $scope.matrimonios.length + $scope.solteros.length + $scope.bajan_poco.length;
+    };
+
+    $scope.Math = window.Math;
+    //$scope.num_personas_grupo = Math.floor($scope.num_personas / $scope.num_grupos);
+    $scope.grupos = [];
+
+    $scope.num_grupos = "6";
+    $scope.hacerGrupos = function () {
+
+        var num_grupos = $scope.num_grupos;
+        var num_personas = $scope.numPersonas();
+        var num_personas_grupo = Math.floor(num_personas / $scope.num_grupos);
+        var matrimonios = $scope.matrimonios;
+        var solteros = $scope.solteros;
+        var bajan_poco = $scope.bajan_poco;
+
+        var contador_personas_grupo = new Array();
+
+        //Creamos num_grupos todavía sin personas
+        var grupos = new Array();
+        for (g = 0; g < num_grupos; g++)
+        {
+            grupos.push(new Array());
+            contador_personas_grupo[g] = 0;
+        }
+
+        //Metemos los matrimonios en los grupos que salgan al azar.
+        for (m = 0; m < matrimonios.length; m++)
+        {
+            var num_grupo = parseInt(Math.random() * num_grupos);
+            grupos[num_grupo].push(matrimonios[m]);
+            contador_personas_grupo[num_grupo] += 2;
+        }
+
+        //Vamos rellenando los grupos con hermanos solteros al azar
+        var copia_solteros = solteros.slice();
+        var copia_bajan_poco = bajan_poco.slice();
+        var barajados = false;
+        for (g = 0; g < num_grupos; g++)
+        {
+            for (p = contador_personas_grupo[g]; p < num_personas_grupo; p++)
+            {
+                s = parseInt(Math.random() * copia_solteros.length);
+                grupos[g].push(copia_solteros[s]);
+                copia_solteros.splice(s, 1); //Elimina al hermano de la lista
+                contador_personas_grupo[g] += 1;
+                if (copia_solteros.length == 0)
+                {
+                    copia_solteros = copia_bajan_poco;
+                    for (gaux = 0; gaux < num_grupos; gaux++)
+                    {
+                        grupos[gaux] = shuffle(grupos[gaux]);
+                    }
+                    barajados = true;
+                }
+            }
+
+        }
+
+        //Los grupos ya están completos pero a lo mejor quedan hermanos sin grupo 
+        if (copia_solteros.length == 0)
+        {
+            copia_solteros = copia_bajan_poco;
+            if (!barajados)
+                for (gaux = 0; gaux < num_grupos; gaux++)
+                {
+                    grupos[gaux] = shuffle(grupos[gaux]);
+                }
+            barajados = true;
+        }
+
+        while (copia_solteros.length > 0)
+        {
+            g = parseInt(Math.random() * num_grupos);
+            if (contador_personas_grupo[g] == num_personas_grupo)
+            {
+                grupos[g].push(copia_solteros[0]);
+                copia_solteros.splice(0, 1);
+                contador_personas_grupo[g] += 1;
+                if (copia_solteros.length == 0)
+                {
+                    copia_solteros = copia_bajan_poco;
+                    if (!barajados)
+                        for (gaux = 0; gaux < num_grupos; gaux++)
+                        {
+                            grupos[gaux] = shuffle(grupos[gaux]);
+                        }
+                    barajados = true;
+                }
+            }
+        }
+        $scope.grupos = grupos;
+    }
+})
+
+.controller('PerfilCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
+
+
+function shuffle(v) {
+    for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x)
+        ;
+    return v;
+}
