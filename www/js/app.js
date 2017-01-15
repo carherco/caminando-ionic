443393c84201a710aea24956b6e2be8bc4df0ddb
Carlos Herrera Conejero <carherco@gmail.com>
2017-01-15 23:14:42 +0100

    ionic en el mac

--- www/js/app.js
+++ www/js/app.js
@@ -13,10 +13,12 @@ angular.module('starter', ['ionic', 'grupos.controllers', 'grupos.services', 'ng
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
+      StatusBar.styleDefault();
     }
   });
 })
--- www/js/services.js
+++ www/js/services.js
@@ -3,4 +3,3 @@ angular.module('grupos.services', [])
         return {};
     });
 
-
